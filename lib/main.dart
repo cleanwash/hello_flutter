21b7a3d2a18115401e641b020cbed445c5b20809
Jayone Lee <kenbugless@gmail.com>
2024-06-17 21:46:37 +0900

    Feat:파일 나누기

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project/my_home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,76 +17,6 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const MyHomePage(),
-    );
-  }
-}
-
-//statefulWidget- 상태가 있는 위젯이라고 생각하면 된다. -> 변화가 있어야 되는 것이 필요할 때
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int count = 0;
-
-  get center => null;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('홈'),
-      ),
-      //body에서, Center로 쌓여있는데, 왜 가운데로 오지 않았냐 -> Center는 이미 가운데로 되어 있음
-      //하지만, Center 영역안에는 Center가 안되어 있음
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              '숫자',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 30,
-              ),
-            ),
-            Text(
-              '$count',
-              style: TextStyle(
-                color: Colors.red,
-                fontSize: 30,
-              ),
-            ),
-            //Elevated Button 사용
-            ElevatedButton(
-              onPressed: () {
-                print('Elevated Button');
-              },
-              child: Text('Elevated Button'),
-            ),
-            TextButton(
-              onPressed: () {
-                print('Text Button');
-              },
-              child: Text('Text Button'),
-            ),
-            OutlinedButton(
-              onPressed: () {},
-              child: Text('OuntLined Button'),
-            )
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            //값이 화면에 랜더링 되려면, setState를 작성하면 좋다.
-            setState(() {
-              count++;
-            });
-          },
-          child: Icon(Icons.add)),
     );
   }
 }
