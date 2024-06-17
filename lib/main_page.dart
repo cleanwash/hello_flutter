d7996f677002640362d905c79a348a5514c878e0
Jayone Lee <kenbugless@gmail.com>
2024-06-18 00:04:56 +0900

    Feat:SingleChildScrollView 적용하기

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -21,81 +21,84 @@ class _MainPageState extends State<MainPage> {
         title: Text('홈'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            //Container는 child를 가질 수 있다.
-            Container(
-              color: Colors.red,
-              width: 100,
-              height: 100,
-            ),
-            SizedBox(
-                height:
-                    50), //단순하게 size를 가져가는 것이라면, container 보단 sizedbox가 낫다, child 속성도 가지고 있다
-            Text(
-              '숫자',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 30,
-              ),
-            ),
-            Text(
-              '$number',
-              style: TextStyle(
+        //화면에 SingleChildScrollView 적용하기
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //Container는 child를 가질 수 있다.
+              Container(
                 color: Colors.red,
-                fontSize: 30,
-              ),
-            ),
-            //Elevated Button 사용
-            ElevatedButton(
-              onPressed: () {
-                print('Elevated Button');
-              },
-              child: Text('Elevated Button'),
-            ),
-            TextButton(
-              onPressed: () {
-                print('Text Button');
-              },
-              child: Text('Text Button'),
-            ),
-            OutlinedButton(
-              onPressed: () {},
-              child: Text('OuntLined Button'),
-            ),
-            //TextField 꾸미기 속성
-            TextField(
-              decoration: InputDecoration(
-                labelText: '글자',
-                border: OutlineInputBorder(),
-              ),
-              onChanged: (text) {
-                print(text);
-              },
-            ),
-            //이미지 추가해보기
-            Image.network(
                 width: 100,
                 height: 100,
-                fit: BoxFit.cover,
-                'https://cdn.huffingtonpost.kr/news/photo/201505/4467_7195.jpeg'),
-            //sizedbox는 색깔이 아닌, 크기만 저장을 할때 사용을 주로 한다.
-            Container(
-              color: Colors.red,
-              width: 120,
-              height: 120,
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Image.asset(
-                  'assets/pic.jpg',
+              ),
+              SizedBox(
+                  height:
+                      50), //단순하게 size를 가져가는 것이라면, container 보단 sizedbox가 낫다, child 속성도 가지고 있다
+              Text(
+                '숫자',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 30,
+                ),
+              ),
+              Text(
+                '$number',
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 30,
+                ),
+              ),
+              //Elevated Button 사용
+              ElevatedButton(
+                onPressed: () {
+                  print('Elevated Button');
+                },
+                child: Text('Elevated Button'),
+              ),
+              TextButton(
+                onPressed: () {
+                  print('Text Button');
+                },
+                child: Text('Text Button'),
+              ),
+              OutlinedButton(
+                onPressed: () {},
+                child: Text('OuntLined Button'),
+              ),
+              //TextField 꾸미기 속성
+              TextField(
+                decoration: InputDecoration(
+                  labelText: '글자',
+                  border: OutlineInputBorder(),
+                ),
+                onChanged: (text) {
+                  print(text);
+                },
+              ),
+              //이미지 추가해보기
+              Image.network(
                   width: 100,
                   height: 100,
                   fit: BoxFit.cover,
+                  'https://cdn.huffingtonpost.kr/news/photo/201505/4467_7195.jpeg'),
+              //sizedbox는 색깔이 아닌, 크기만 저장을 할때 사용을 주로 한다.
+              Container(
+                color: Colors.red,
+                width: 120,
+                height: 120,
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Image.asset(
+                    'assets/pic.jpg',
+                    width: 100,
+                    height: 100,
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
